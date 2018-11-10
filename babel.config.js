6fd8e415d49f17030bb3faf3da35923bcfda855c
Subash Pathak <subash@subash.me>
2018-11-10 11:32:27 +0545

    Initial Commit

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: [
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-transform-modules-commonjs'
+  ]
+}
---
+++ tests/mkcert.test.js
@@ -0,0 +1,1 @@
+import mkcert from '../src/mkcert';
